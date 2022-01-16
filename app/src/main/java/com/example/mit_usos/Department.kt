d3306a9b876f87ce4c94208fceac9f4ce23cc9f1
soczil <ksoczewica@gmail.com>
2022-01-16 17:31:52 +0100

    widok planu i wydzialu

---
+++ app/src/main/java/com/example/mit_usos/Department.kt
@@ -0,0 +1,11 @@
+package com.example.mit_usos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Department : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_department)
+    }
+}
--- app/src/main/java/com/example/mit_usos/ui/schedule/ScheduleFragment.kt
+++ app/src/main/java/com/example/mit_usos/ui/schedule/ScheduleFragment.kt
@@ -1,42 +1,29 @@
 package com.example.mit_usos.ui.schedule
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.mit_usos.databinding.FragmentScheduleBinding
+import com.example.mit_usos.Department
+import com.example.mit_usos.R
+import com.example.mit_usos.ui.gallery.GalleryFragment
 
 class ScheduleFragment : Fragment () {
-    private lateinit var scheduleViewModel: ScheduleViewModel
-    private var _binding: FragmentScheduleBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        scheduleViewModel = ViewModelProvider(this).get(ScheduleViewModel::class.java)
+        val view = inflater.inflate(R.layout.fragment_schedule, container, false)
+        val button = view.findViewById<View>(R.id.button)
 
-        _binding = FragmentScheduleBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textSchedule
-        scheduleViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        button.setOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, Department::class.java)
+            startActivity(intent)
         })
-        return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return view
     }
 }
